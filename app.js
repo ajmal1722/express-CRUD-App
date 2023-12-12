ca68c7cf2c855bbdbfd4f608de0d963c65204ddc
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-12-12 15:01:27 +0530

    used  get requests to get the datas of all members and single member

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 // Importing Packages
 const express = require('express');
-const app = express()
+const fs = require('fs');
+const path = require('path');
+
+
+// inbuild modules
+const members = require('./datas/members.js');
+const { error } = require('console');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Reading Files
+const indexPath = fs.readFileSync('./templates/index.ejs', 'utf-8');
 
 // ROUT = HTTP METHOD + URL
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World')
+
+// Get All members
+app.get('/api/members', (req, res) => {
+    try {
+        res.json(members) 
+    } catch (error) {
+        console.log('Error1 : ', error)
+    }
+});
+
+// Get Single members
+app.get('/api/members/:id', (req, res) => {
+    try {
+        const found = members.some(member => member.id === parseInt(req.params.id));
+        if (found) {
+            res.json(members.filter(member => member.id === parseInt(req.params.id)));    
+        } else {
+            res.status(400).send(`NO Member with Id of ${req.params.id}`)
+        }
+
+    } catch (error) {
+        console.log('Error2 : ', error)
+    }
 })
+
 // Creating server
-const port = 3000;
-app.listen(port, () => {
-    console.log('server has created..')
-})
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
---
+++ datas/members.js
@@ -0,0 +1,17 @@
+const members = [
+    {
+        id : 1,
+        name : 'Ajmal',
+        age : 22
+    },{
+        id : 2,
+        name : 'jon',
+        age : 34
+    },{
+        id : 3,
+        name : 'katy',
+        age : 25
+    }
+];
+
+module.exports = members;
